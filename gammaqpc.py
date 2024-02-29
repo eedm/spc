a2fc0407cbaf53d0f049244895aea51af3e1c347
eedm <eedm105@gmail.com>
2024-02-29 13:25:03 -0800

    correct handling of units and exceptions

--- gammaqpc.py
+++ gammaqpc.py
@@ -1,8 +1,14 @@
+#
+# Read and print ion pump presures for Gamma Small Pump Controller - SPCe
+# Interface uses socket library to open a telnet connection to the pump controllers 1-4.
+# CT 2-17-24
+from datetime import datetime
 import socket
+import traceback
 
 def fetchGammaPressure(host, pumpindex):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.settimeout(2)
+	s.settimeout(4)
 
 	try:
 		s.connect((host, 23))
@@ -12,7 +18,11 @@ def fetchGammaPressure(host, pumpindex):
 
 	# First fetch prompt
 	while True:
-		chunk = s.recv(1)
+		try:
+			chunk = s.recv(1)
+		except socket.error as e:
+			print(f"socket error: {e} trying to fetch prompt")
+			s.close()
 		if chunk == b'':
 			print('Failed to receive')
 			s.close()
@@ -21,12 +31,19 @@ def fetchGammaPressure(host, pumpindex):
 			break
 
 	# Received prompt, transmit our command
-	s.send(('spc 0B '+str(pumpindex)+"\r\n").encode())
-
+	try:
+		s.send(('spc 0B '+str(pumpindex)+"\r\n").encode())
+	except socket.error as e:
+		print(f"socket error: {e} trying to send query")
+		s.close()
+		return False
 	# Wait for reply and read till end of line marker
 	repl = ''
 	while True:
-		chunk = s.recv(1)
+		try:
+			chunk = s.recv(1)
+		except socket.error as e:
+                        print(f"socket error: {e} trying to read from controller")
 		if chunk == b'':
 			print('Failed to receive')
 			s.close()
@@ -49,8 +66,9 @@ def fetchGammaPressure(host, pumpindex):
 		print("Failed to read response from GammaQPC")
 		return False
 
-	if(repl[3] != 'MBAR\r\r\n'):
-		print("Gamma QPC not set to MBAR")
+	if(repl[3] != 'TORR\r\r\n'):
+		print("Gamma QPC not set to TORR")
+		print(repl[3])
 		return False
 
 	pressure = float(repl[2])
@@ -60,7 +78,7 @@ if __name__ == "__main__":
   import sys
 
   if(len(sys.argv) < 3):
-    print("Usage: "+sys.argv[0]+" HOSTNAME PORT")
+    print("Usage: "+sys.argv[0]+" HOSTNAME INDEX[1-4]")
   else:
     try:
       host = sys.argv[1]
@@ -73,9 +91,16 @@ if __name__ == "__main__":
       pressure = fetchGammaPressure(host, index)
 
       if(pressure == 1.3e-11):
-        print("Result is 1.3e-11 mbar, this is also signalled if the QPC has no pump attached ...")
+        print("Result is 1.3e-11 torr, this is also signalled if the QPC has no pump attached ...")
         sys.exit()
 
-      print(str(pressure)+" mbar")
+      if(pressure == 0.1e-10):
+        print("Reading 0.1e-10 torr means HV OFF ...")
+        sys.exit()
+
+      now = datetime.now()
+      print(now.strftime('%Y-%m-%d %H:%M:%S')+"  "+str(pressure)+" torr")
     except:
         print("Failed to query host "+host+" for pump "+str(index))
+        traceback.print_exc()
+
